e1a4c36acbd3ec10e502494d3cbd8a0a48b978f1
Habibur Rahman <hrekns@gmail.com>
2026-06-20 20:51:37 +0300

    Added into main some primary changes

--- src/main/java/dnsolutions/fi/SelfDevelopment/dto/AuthResponseDTO.java
+++ src/main/java/dnsolutions/fi/SelfDevelopment/dto/AuthResponseDTO.java
@@ -1,5 +1,6 @@
 package dnsolutions.fi.SelfDevelopment.dto;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -11,4 +12,12 @@ public class AuthResponseDTO {
     private String accessToken;
     private String tokenType;
     private UserDTO user;
+    @JsonIgnore
+    private String refreshToken;
+
+    public AuthResponseDTO(String accessToken, String tokenType, UserDTO user) {
+        this.accessToken = accessToken;
+        this.tokenType = tokenType;
+        this.user = user;
+    }
 }
